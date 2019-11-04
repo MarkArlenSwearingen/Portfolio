9950ddfcdf74c2670c86577035bf4ba61e3400ef
Mark Swearingen <markswearingen@bendcable.com>
2019-11-04 11:35:46 -0800

    Create carousel function

--- assets/js/main.js
+++ assets/js/main.js
@@ -399,3 +399,27 @@
 					});
 
 })(jQuery);
+
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow_auto
+//Carousel Function
+let slideIndex = 0;
+carousel();
+
+function carousel() {
+let i;
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
+for ( i = 0; i < slides.length; i++){
+  slides[i].style.display="none";
+}
+slideIndex++;
+if (slideIndex > slides.length){
+  slideIndex = 1
+}
+for (i = 0; i < dots.length; i++){
+  dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " active";
+setTimeout(showSlides, 2000);
+}
